469a27e03e7aa03527672f5d3b6db2838bffe831
9adevteam <126467794+9adevteam@users.noreply.github.com>
2023-02-27 14:11:17 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include<stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	printf("The Hello World (made in trash school pc)");
+	return 0;
+}
